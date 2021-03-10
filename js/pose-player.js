d4d3227b2a8e4d38b97c056ea47f9317c0e43bf6
Jeff Shepherd <jeffindenver2002@yahoo.com>
2021-03-09 19:49:22 -0600

    first working version

--- js/pose-player.js
+++ js/pose-player.js
@@ -1,25 +1,49 @@
 "use strict";
 
 $(function () {
+
+  let duration = 5000;
+  let index = 1;
+  let timerID;
+  let images = [];
+  let currentPose = $("#image-target");
+  let pose = $('<img>');
+
   //jquery events
   $(".pickDirectory").change((event) => {
 
-    let target = $("#output-target");
     let files = event.target.files;
-    let images = [];
-
     console.log("Number of files: " + files.length);
 
     for (let i = 0; i < files.length; i++) {
       images.push(URL.createObjectURL(files[i]));
     }
-
     // attempt to display the first image
-    let currentPose = $("#image-target");
-    let pose = $('<img>');
-
-    pose.attr("src", images[2]);
     currentPose.append(pose);
+    // load first image before timer starts
+    pose.attr("src", images[0]);
 
   });
+
+  $(".pause").click( () => {
+    clearInterval(timerID);
+    console.log("Timer stopped");
+  });
+
+  // play each image for a duration of time
+  $(".play").click(startTimer);
+
+  function poseTimer() {
+    if (index === images.length) {
+      index = 0;
+    }
+    pose.attr("src", images[index]);
+    index++;
+  }
+
+  function startTimer() {
+    timerID = setInterval(poseTimer, duration);
+    console.log("Timer started");
+  }
+
 });

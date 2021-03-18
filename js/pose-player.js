664f560f80f817cd703e8cbea068a02bbf225bc1
Jeff Shepherd <jeffindenver2002@yahoo.com>
2021-03-18 06:46:12 -0500

    added randomization feature

--- js/pose-player.js
+++ js/pose-player.js
@@ -5,9 +5,12 @@ $(function () {
   let duration = 60;
   let index = 1;
   let timerID;
-  let images = [];
+  let originalImageList = [];
+  let currentImageList = [];
   let currentPose = $("#image-target");
   let pose = $('<img>');
+  let isRandom = true;
+
 
   // alternative timer
   /*
@@ -15,28 +18,28 @@ $(function () {
     setInvterval for the clock. When the clock is equal to the selected
     duration, change the image and reset the clock to zero and repeat.
     This eliminates the issue with coordinating two intervals.
-  */
 
-  /*
-    @TODO Create a randomize function. 1. make deep copy of array. 2. Iterate
-    over entire array and swap each element with a random element from 0 to the
-    length of the array.
-   */
+    Another alternative is to use setTimeout. Then have setTimeout inoke the next
+    timer.
+  */
 
   //jquery events
   $(".pickDirectory").change((event) => {
 
+    // assign the list of directory files
     let files = event.target.files;
     console.log("Number of files: " + files.length);
 
+    // Iterate over files and convert them to URL objects
     for (let i = 0; i < files.length; i++) {
-      images.push(URL.createObjectURL(files[i]));
+    originalImageList.push(URL.createObjectURL(files[i]));
     }
 
-    images.forEach(console.log);
+    // copy original list
+    currentImageList = originalImageList;
 
     currentPose.append(pose);
-    pose.attr("src", images[0]);
+    pose.attr("src", currentImageList[0]);
 
     $(".notifications").html("Select a time period for your quick poses. \
       Then hit play. The default period is 60 seconds.");
@@ -46,26 +49,70 @@ $(function () {
     $(this).blur();
   });
 
-  $(".pause").click( () => {
+
+  $(".pause").click(pauseTimer);
+
+  $(".play").click(beginPlayer);
+
+  function beginPlayer () {
+    //TODO: check imageList length, if 0 alert that a directory must be selected
+    resetIndex();
+    getCurrentList();
+    startTimer(getMilliseconds());
+  }
+
+  function pauseTimer() {
     clearInterval(timerID);
     $(".notifications").html("Pose Player paused");
     console.log("Timer stopped");
-  });
-
-  $(".play").click(initTimer);
+  }
 
   $("input[type='radio'][name='timing']").click( () => {
     duration = getSelectedTime();
   });
 
-  function initTimer() {
+  $("input[type='checkbox'][name='randomizer']").change ( () => {
+    // check if the list should be randomized
+    if ($("input[type='checkbox'][name='randomizer']:checked").val()) {
+      isRandom = true;
+      console.log("randomizer is checked");
+    } else {
+      isRandom = false;
+      console.log("randomizer is unchecked");
+    }
+  });
+
+  function getCurrentList() {
+    if(isRandom) {
+      randomizeList();
+    } else {
+      currentImageList = originalImageList;
+    }
+  }
+
+  function getMilliseconds() {
     let milliseconds = duration * 1000;
     console.log("milliseconds: " + milliseconds);
-    startTimer(milliseconds);
+    return milliseconds;
+  }
+
+  function randomizeList() {
+    // swap the elements in place using random number gen
+    for (let i = 0; i < currentImageList.length; i++) {
+      let holdingBucket = originalImageList[i];
+      let randomIndex = getRandomNum(currentImageList.length - 1);
+      currentImageList[i] = originalImageList[randomIndex];
+      currentImageList[randomIndex] = holdingBucket;
+    }
+    console.log("current image list length is " + currentImageList.length);
+  }
+
+  function getRandomNum (max) {
+    return Math.floor(Math.random() * max);
   }
 
   function startTimer(milliseconds) {
-    timerID = setInterval(poseTimer, milliseconds);
+    timerID = setInterval(changeImage, milliseconds);
     console.log("Timer started");
 
     flashElement($(".notifications"));
@@ -73,12 +120,16 @@ $(function () {
     $(".notifications").html("Pose Player playing");
   }
 
-  function poseTimer() {
-    if (index === images.length) {
-      index = 0;
+  function changeImage() {
+    if (index === currentImageList.length) {
+      pauseTimer();
     }
-    pose.attr("src", images[index]);
+    pose.attr("src", currentImageList[index]);
     index++;
+  }
+
+  function resetIndex() {
+    index = 1;
   }
 
   function flashElement(element) {

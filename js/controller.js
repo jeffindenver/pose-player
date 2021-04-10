cb010274ec8eb5342fad7cd92219791c9c60dea8
Jeff Shepherd <jeffindenver2002@yahoo.com>
2021-04-09 21:21:14 -0500

    separated data from controller

---
+++ js/controller.js
@@ -0,0 +1,125 @@
+"use strict";
+
+$(function () {
+
+  let pp = new PosePlayer();
+
+  // TODO: Improve clock visuals
+
+   const FORWARD = 1;
+   const BACKWARD = -1;
+
+  /*****************************************************************************
+   * jQuery events
+   ****************************************************************************/
+  $(".pickDirectory").change((event) => {
+    const files = event.target.files;
+    pp.resetOriginalList();
+    pp.buildImageList(files);
+    if(pp.isRandomChecked) {
+      pp.randomizeList();
+    }
+    loadFirstImage();
+    notify("Select a time period for your quick poses. Then hit play.");
+    $(this).blur();
+  });
+
+  $(".pause").click(pauseTimer);
+
+  $(".play").click(beginPlayer);
+
+  $(".stepforward").click(stepForward);
+
+  $(".stepbackward").click(stepBackward);
+
+  $("input[type='radio'][name='timing']").click(() => {
+    pp.duration = getSelectedTime();
+  });
+
+  $("input[type='checkbox'][name='randomizer']").change(() => {
+    pp.isRandomChecked = $(this.checked).val();
+  });
+
+  /*****************************************************************************
+   * Controller functions
+   ****************************************************************************/
+  function beginPlayer() {
+    if (pp.currentImageList.length < 1) {
+      notify("Please first select a directory.");
+      return;
+    }
+    notify("Pose Player playing");
+    startTimer();
+  }
+
+  function pauseTimer() {
+    clearInterval(pp.clockID);
+    notify("Pose Player paused");
+    console.log("Timer stopped");
+  }
+
+  function stepForward() {
+    clearInterval(pp.clockID);
+    changeImage(FORWARD);
+    startTimer();
+  }
+
+  function stepBackward() {
+    if (pp.index === 0) {
+      return;
+    }
+    clearInterval(pp.clockID);
+    changeImage(BACKWARD);
+    startTimer();
+  }
+
+  function loadFirstImage() {
+    pp.currentPose.append(pp.pose);
+    pp.pose.attr("src", pp.currentImageList[0]);
+  }
+
+  function startTimer() {
+    pp.timeLeft = pp.duration;
+    clearInterval(pp.clockID);
+    pp.clockID = setInterval(() => {
+      pp.timeLeft--;
+      updateClock(pp.timeLeft);
+      if (pp.timeLeft === 0) {
+        clearInterval(pp.clockID);
+        changeImage(FORWARD);
+      }
+    }, 1000);
+  }
+
+  function changeImage(direction) {
+    pp.index += direction;
+
+    if (pp.index >= pp.currentImageList.length) {
+      pp.index = 0;
+    }
+    if (pp.index < 0) {
+      pp.index = pp.currentImageList.length - 1;
+    }
+    pp.pose.attr("src", pp.currentImageList[pp.index]);
+    console.log("displaying image " + pp.index);
+    startTimer();
+  }
+
+  function updateClock(timeLeft) {
+    $(".clock").html(timeLeft);
+  }
+
+  function notify(message) {
+    $(".notifications").css("display", "block");
+    flashElement($(".notifications"));
+    $(".notifications").html(message);
+  }
+
+  function flashElement(element) {
+    element.fadeOut(50).fadeIn(50).fadeOut(400).fadeIn(600);
+  }
+
+  function getSelectedTime() {
+    return $("input[type='radio'][name='timing']:checked").val();
+  }
+});
--- js/pose-player.js
+++ js/pose-player.js
@@ -1,185 +1,59 @@
-"use strict";
+/*******************************************************************************
+ * Title: pose-player.js
+ * Author: Jeff Shepherd
+ * Date: 4/9/2021
+ * Description: PosePlayer object that holds data on the current list of
+ * images, index for current pose, time remaining and related functions.
+ ******************************************************************************/
 
-$(function () {
+function PosePlayer() {
+  "use strict";
 
-  // TODO: put the following variables in a PosePlayer object. Move functions
-  // that belong there.
+  this.duration = 60;
+  this.timeLeft;
+  this.index = 0;
+  this.clockID;
+  this.currentPose = $("#image-target");
+  this.pose = $('<img>');
+  this.isRandomChecked = true;
+  this.originalImageList = [];
+  this.currentImageList = [];
 
-  // TODO: Improve clock visuals
+  this.resetOriginalList = function() {
+    this.originalImageList = [];
+  }
 
-  // TODO: Improve pause function so that pausing does not reset the list
-  // and continues where it left off.
-
-  let duration = 60;
-  let index = 0;
-  let clockID;
-  let originalImageList = [];
-  let currentImageList = [];
-  let currentPose = $("#image-target");
-  let pose = $('<img>');
-  let isRandomized = true;
-  const FORWARD = 1;
-  const BACKWARD = -1;
-
-  /*******************************************************************************
-   * jQuery events
-   ******************************************************************************/
-  $(".pickDirectory").change((event) => {
-    const files = event.target.files;
-    resetOriginalList();
-    buildImageList(files);
-    setCurrentList();
-    notify("Select a time period for your quick poses. Then hit play.");
-    $(this).blur();
-  });
-
-  $(".pause").click(pauseTimer);
-
-  $(".play").click(beginPlayer);
-
-  $(".stepforward").click(stepForward);
-
-  $(".stepbackward").click(stepBackward);
-
-  $("input[type='radio'][name='timing']").click(() => {
-    duration = getSelectedTime();
-  });
-
-  $("input[type='checkbox'][name='randomizer']").change(() => {
-    isRandomized = $(this.checked).val();
-  });
-
-  /*****************************************************************************
-   * other functions
-   ****************************************************************************/
-  function buildImageList(files) {
+  this.buildImageList = function(files) {
     for (let i = 0; i < files.length; i++) {
       if (isValidImage(files[i].type)) {
         console.log("added: " + files[i].type);
-        originalImageList.push(URL.createObjectURL(files[i]));
+        this.originalImageList.push(URL.createObjectURL(files[i]));
       }
     }
+    this.currentImageList = this.originalImageList;
   }
 
   function isValidImage(type) {
     return (type === `image/jpeg` || type === `image/png`);
   }
 
-  function beginPlayer() {
-    if (currentImageList.length < 1) {
-      notify("Please first select a directory.");
-      return;
-    }
-    notify("Pose Player playing");
-    resetIndex();
-    getCurrentList();
-    loadFirstImage();
-    startTimer();
+  this.getCurrentList = function () {
+    return this.currentImageList;
   }
 
-  function pauseTimer() {
-    clearInterval(clockID);
-    notify("Pose Player paused");
-    console.log("Timer stopped");
-  }
-
-  function stepForward() {
-    clearInterval(clockID);
-    changeImage(FORWARD);
-    startTimer();
-  }
-
-  function stepBackward() {
-    if (index === 0) {
-      return;
-    }
-    clearInterval(clockID);
-    changeImage(BACKWARD);
-    startTimer();
-  }
-
-  function loadFirstImage() {
-    currentPose.append(pose);
-    pose.attr("src", currentImageList[0]);
-  }
-
-  function getCurrentList() {
-    if (isRandomized) {
-      randomizeList();
-    } else {
-      setCurrentList();
-    }
-  }
-
-  function randomizeList() {
+  this.randomizeList = function () {
     // swap the elements in place using random number gen
-    for (let i = 0; i < currentImageList.length; i++) {
-      let holdingBucket = originalImageList[i];
-      let randomIndex = getRandomNum(currentImageList.length - 1);
-      currentImageList[i] = originalImageList[randomIndex];
-      currentImageList[randomIndex] = holdingBucket;
+    for (let i = 0; i < this.currentImageList.length; i++) {
+      let holdingBucket = this.originalImageList[i];
+      let randomIndex = getRandomNum(this.currentImageList.length - 1);
+      this.currentImageList[i] = this.originalImageList[randomIndex];
+      this.currentImageList[randomIndex] = holdingBucket;
     }
-    console.log("current image list length is " + currentImageList.length);
+    console.log("current image list length is " + this.currentImageList.length);
   }
 
   function getRandomNum(max) {
     return Math.floor(Math.random() * max);
   }
+}
 
-  function startTimer() {
-    clearInterval(clockID);
-    let timeLeft = duration;
-    clockID = setInterval(() => {
-      timeLeft--;
-      updateClock(timeLeft);
-      if (timeLeft === 0) {
-        clearInterval(clockID);
-        changeImage(FORWARD);
-      }
-    }, 1000);
-  }
-
-  function changeImage(direction) {
-    index += direction;
-
-    if (index >= currentImageList.length) {
-      index = 0;
-    }
-    if (index < 0) {
-      index = currentImageList.length - 1;
-    }
-    pose.attr("src", currentImageList[index]);
-    console.log("displaying image " + index);
-    startTimer();
-  }
-
-  function updateClock(timeLeft) {
-    $(".clock").html(timeLeft);
-  }
-
-  function notify(message) {
-    $(".notifications").css("display", "block");
-    flashElement($(".notifications"));
-    $(".notifications").html(message);
-  }
-
-  function resetIndex() {
-    index = 0;
-  }
-
-  function resetOriginalList() {
-    originalImageList = [];
-  }
-
-  function setCurrentList() {
-    currentImageList = originalImageList;
-  }
-
-  function flashElement(element) {
-    element.fadeOut(50).fadeIn(50).fadeOut(400).fadeIn(600);
-  }
-
-  function getSelectedTime() {
-    return $("input[type='radio'][name='timing']:checked").val();
-  }
-});
